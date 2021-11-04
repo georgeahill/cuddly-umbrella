d29159df022928e5e0cad840ac93c1d2616a73d9
Ben Skinner <benskinner1999@gmail.com>
2021-11-04 15:22:31 +0000

    Created base wordcloud API

---
+++ Docs/Wikipedia API.md
@@ -0,0 +1,26 @@
+# Wikipedia API Specification
+Reference taken from [here](https://www.mediawiki.org/wiki/API:REST_API/Reference)
+
+root is `https://en.wikipedia.org/w/rest.php/v1`
+
+## Page search
+`/search/page?q={search query}&limit={Limit (1)}`
+
+**Returns:** 
+```json
+{
+  "id": 38930,
+  "key": "Jupiter",
+  "title": "Jupiter",
+  "excerpt": "<span class=\"searchmatch\">Jupiter</span> is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half",
+  "description": "fifth planet from the Sun and largest planet in the Solar System",
+  "thumbnail": {
+    "mimetype": "image/jpeg",
+    "size": null,
+    "width": 200,
+    "height": 200,
+    "duration": null,
+    "url": "//upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Jupiter_and_its_shrunken_Great_Red_Spot.jpg/200px-Jupiter_and_its_shrunken_Great_Red_Spot.jpg"
+  }
+}```
+
--- WordCloud/app.py
+++ WordCloud/app.py
@@ -1,6 +1,63 @@
-from flask import Flask
+from flask import Flask, request
+import re
+from wordcloud import WordCloud, ImageColorGenerator
+import matplotlib.pyplot as plt
+from PIL import Image
+import base64
+from io import BytesIO
+
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["POST"])
 def index():
-    return "Hello, World!"
+    data = request.get_json()
+    print(data)
+
+    data1 = "I am tired! I don't like fruit...and milk"
+
+    cleanedInput = re.sub(r"""
+               [,.;@#?!&$]+  # Accept one or more copies of punctuation
+               \ *           # plus zero or more copies of a space,
+               """,
+               " ",          # and replace it with a single space
+               data1, flags=re.VERBOSE)
+
+
+    #Define a list of stop words
+    stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 
+    'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 
+    'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what',
+    'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were',
+    'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an',
+    'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for',
+    'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above',
+    'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further',
+    'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few',
+    'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than',
+    'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
+
+    #generate the word cloud from text
+    cloud = WordCloud(width=400,
+                      height=330,
+                      max_words=150,
+                      colormap='tab20c',
+                      stopwords=stopwords,
+                      collocations=True).generate_from_text(cleanedInput)
+
+    im = Image.fromarray(cloud.to_array())
+
+    buffered = BytesIO()
+    im.save(buffered, format="JPEG")
+    img_str = base64.b64encode(buffered.getvalue())
+    
+
+    # plt.figure(figsize=(10,8))
+    # plt.imshow(cloud)
+    # plt.axis('off')
+    # plt.title("Wordcloud", fontsize=13)
+    # plt.savefig('fig.png')
+
+
+
+    return img_str
